4fcd0111eb64fc401683d67b7801f674fe8a76d2
tyagi-iiitv <anjul.ten@gmail.com>
2016-06-07 23:58:11 +0530

    Added the final 2262 code

--- 2262.cpp
+++ 2262.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
+#include <math.h>
 using namespace std;
 
 bool isPrime[1000001];
 void generate_primes(bool isPrime[], int len){
 	isPrime[1]=false;
 	isPrime[0]=false;
-	for(int i=2;i<=sqrt(len);i++){
+	for(int i=2;i<=sqrt((double)len);i++){
 		if(isPrime[i]==true){
 			for(int j=i*i;j<=len;j=j+i){
 				isPrime[j]=false;
@@ -16,24 +16,24 @@ void generate_primes(bool isPrime[], int len){
 	}
 }
 
-void solve(int n, queue<int> ans){
-	if(isPrime[n])
-		ans.push(n);
-	else
-		for(int i=3; i<=n;i++){
-			if(isPrime[i])
+void solve(int n){
+	for(int i=3;i<=n;i++){
+		if(isPrime[i] && isPrime[n-i]){
+			cout<<n<<" = "<<i<<" + "<<n-i<<endl;
+			break;
 		}
+	}
 }
 
 int main(){
 	ios::sync_with_stdio(false);
-	fill_n(isPrime.begin(), isPrime.end(), true);
+	fill_n(isPrime, 1000001, true);
 	generate_primes(isPrime, 1000001);
 	int n;
 	cin>>n;
 	while(n != 0){
-		queue<int> ans;
-		solve(n, ans);
+		solve(n);
+		cin>>n;
 	}
 	return 0;
 }
